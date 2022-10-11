20443e7d6696423deaf83c3b2f52c265add7a6fa
Wolfgang Corcoran-Mathe <wcm@sigwinch.xyz>
2022-10-11 11:17:15 -0400

    Add exports and imports.

--- srfi.217.internal.scm
+++ srfi.217.internal.scm
@@ -31,6 +31,21 @@
 ;;; This means that the empty trie (#f) never appears as a subtrie.
 
 (module (srfi 217 internal)
+  (trie-contains? trie-min trie-max trie-insert trie-fold trie-fold-right
+   trie-partition trie-filter trie-delete trie-delete-min trie-delete-max
+   trie-search trie-union trie-xor trie-intersection trie-difference
+   copy-trie trie-size trie=? trie-subset-compare trie-proper-subset?
+   trie-disjoint? subtrie< subtrie> subtrie-interval
+   )
+
+(import scheme
+        (chicken base)
+        (chicken condition)
+        (chicken type)
+        (srfi 143)
+        typed-records
+        )
+
 (define-record-type <leaf>
   (raw-leaf prefix bitmap)
   leaf?
@@ -938,3 +953,5 @@
                    (else (fxand low-mask bitmap))))
             ((fx>? prefix hp) 0)
             (else (fxand (fxand low-mask high-mask) bitmap))))))
+
+)
